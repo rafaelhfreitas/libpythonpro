1afc5bc103513e1f78291556a2bb381be1481091
Rafael Henrique <oraffaudi@gmail.com>
2019-11-04 20:25:27 -0300

    Create requirements file

---
+++ libpythonpro/github_api.py
@@ -0,0 +1,15 @@
+import requests
+
+def get_avatar(user):
+    """ Busca o avatar de usuário no Github
+
+    :param usuario: str com o nome do usuário
+    :return: str com o link do avatar do usuário
+    """
+    url=f'https://api.github.com/users/{user}'
+    resp = requests.get(url)
+    return resp.json()['avatar_url']
+
+
+if __name__ == '__main__':
+    print(get_avatar('rafaelhfreitas'))
---
+++ libpythonpro/test_github_api.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+from libpythonpro import github_api
+
+class TestGitHubApi(TestCase):
+    def test_get_avatar(self):
+        user = 'rafaelhfreitas'
+
+        url_received = github_api.get_avatar(user)
+        url_expected = "https://avatars3.githubusercontent.com/u/21064382?v=4"
+
+        self.assertEqual(url_expected, url_received)
