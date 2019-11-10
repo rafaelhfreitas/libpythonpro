40df046ae189183fd38d52e319f226f81e66b92a
Rafael Henrique <oraffaudi@gmail.com>
2019-11-09 22:04:57 -0300

    Refactoring to build with flake8

--- libpythonpro/tests/test_github_api.py
+++ libpythonpro/tests/test_github_api.py
@@ -1,21 +1,6 @@
-from unittest import TestCase
 from libpythonpro import github_api
-
 
 
 def test_get_avatar():
     url = github_api.get_avatar('rafaelhfreitas')
     assert 'https://avatars3.githubusercontent.com/u/21064382?v=4' == url
-
-
-# class TestGitHubApi(TestCase):
-#     def test_get_avatar(self):
-#         user = 'rafaelhfreitas'
-#
-#         url_received = github_api.get_avatar(user)
-#         url_expected = "https://avatars3.githubusercontent.com/u/21064382?v=4"
-#
-#         self.assertEqual(url_expected, url_received)
-#
-#     def test_init(self):
-#         assert 1 == 1
