f9bca5e72186cc29890300d971a596e0cb521128
Rafael Henrique <oraffaudi@gmail.com>
2019-11-09 22:39:38 -0300

    Isolation test with fixture

--- libpythonpro/tests/test_github_api.py
+++ libpythonpro/tests/test_github_api.py
@@ -1,6 +1,30 @@
+from unittest.mock import Mock
+
+import pytest
+
 from libpythonpro import github_api
 
 
-def test_get_avatar():
+@pytest.fixture
+def avatar_url():
+    resp_mock = Mock()
+    url = 'https://avatars3.githubusercontent.com/u/21064382?v=4'
+    resp_mock.json.return_value = {
+        'login': 'rafaelhfreitas',
+        'id': 21064382,
+        'avatar_url': url
+    }
+    original_get = github_api.requests.get
+    github_api.requests.get = Mock(return_value=resp_mock)
+    yield url
+    github_api.requests.get = original_get
+
+
+def test_get_avatar(avatar_url):
+    url = github_api.get_avatar('rafaelhfreitas')
+    assert avatar_url == url
+
+
+def test_get_avatar_integration():
     url = github_api.get_avatar('rafaelhfreitas')
     assert 'https://avatars3.githubusercontent.com/u/21064382?v=4' == url
